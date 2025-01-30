cf67ea378f735bdaf92143fe7e9dd0626d6f30b6
anniechakraborty <the.demented.genius29@gmail.com>
2025-01-30 23:52:22 +0100

    removed config file

--- app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', '')
-    MONGO_URI = os.getenv('MONGO_URI', 'mongodb://localhost:27017')
-    JWT_SECRET_KEY = os.getenv('JWT_SECRET_KEY', '')
